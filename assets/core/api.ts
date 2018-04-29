63234337e82d64c6c0a60819cbf4c04c3ada4c2f
varunchandwani <varunchandwani@gmail.com>
2018-04-29 22:15:35 +1000

    make it better

---
+++ assets/core/api.ts
@@ -0,0 +1,18 @@
+export default {
+    host : 'https://sheltered-caverns-71469.herokuapp.com/',
+    message: 'message',
+    user: 'user',
+    userSignIn: 'user/signin',
+    portfolio: 'portfolio',
+    portfolioNames: 'portfolio/names',
+    mutualFundNames: 'portfolio/mutualFundNames',
+    portfolioMonthly: 'portfolio/monthly',
+    fundWise: 'portfolio/activeFunds',
+    fundLastEntry: 'portfolio/fundLastEntry',
+    lastEntry: 'portfolio/lastEntry',
+    zeroLastEntry: 'portfolio/zeroLastEntry',
+    quandlApi: 'https://www.quandl.com/api/v3/datasets/AMFI/XXX.json?column_index=1&api_key=QAcRRkev76MWjxja2kYd',
+    quandlStockApi: 'https://www.quandl.com/api/v3/datasets/NSE/XXX.json?api_key=QAcRRkev76MWjxja2kYd',
+    updatePrice: 'portfolio/updatePrice',
+    dataForDates: 'portfolio/dataForDates'
+};
