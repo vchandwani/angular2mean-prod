9a68529eb8f710c31c795fa62d6b178798051a88
varunchandwani <varunchandwani@gmail.com>
2018-05-01 15:52:06 +1000

    Changes for Appmodule on 08-Optimization

--- assets/app/app.module.js
+++ assets/app/app.module.js
@@ -4,6 +4,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpModule } from "@angular/http";
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Ng2OdometerModule } from 'ng2-odometer';
+import { CalendarModule } from 'primeng/calendar';
+import { SelectModule } from 'ng2-select';
 import { AppComponent } from "./app.component";
 import { AuthenticationComponent } from "./auth/authentication.component";
 import { HeaderComponent } from "./header.component";
@@ -11,6 +14,10 @@ import { routing } from "./app.routing";
 import { AuthService } from "./auth/auth.service";
 import { ErrorComponent } from "./errors/error.component";
 import { ErrorService } from "./errors/error.service";
+import { DataTableModule, SharedModule, DialogModule, DataGridModule, InputTextModule, ButtonModule } from 'primeng/primeng';
+import { TableModule } from 'primeng/table';
+import { Ng4LoadingSpinnerModule } from 'ng4-loading-spinner';
+import { ChartsModule } from 'ng2-charts';
 import { MessageModule } from "./messages/message.module";
 var AppModule = /** @class */ (function () {
     function AppModule() {
@@ -30,6 +37,19 @@ var AppModule = /** @class */ (function () {
                         BrowserAnimationsModule,
                         routing,
                         HttpModule,
+                        // PortfolioDetailModule,
+                        Ng4LoadingSpinnerModule.forRoot(),
+                        Ng2OdometerModule.forRoot(),
+                        ChartsModule,
+                        DataTableModule,
+                        DataGridModule,
+                        ButtonModule,
+                        TableModule,
+                        InputTextModule,
+                        SharedModule,
+                        DialogModule,
+                        CalendarModule,
+                        SelectModule,
                         MessageModule
                     ],
                     providers: [AuthService, ErrorService
