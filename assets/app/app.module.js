d3ad963a901e1fcd1be031acbe3a8a8892ba4732
varunchandwani <varunchandwani@gmail.com>
2018-05-01 15:44:29 +1000

    App Module changes

--- assets/app/app.module.js
+++ assets/app/app.module.js
@@ -1,6 +1,13 @@
-import { NgModule } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { NgModule, ApplicationRef } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpModule } from "@angular/http";
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Ng2OdometerModule } from 'ng2-odometer';
+import { CalendarModule } from 'primeng/calendar';
+import { SelectModule } from 'ng2-select';
 import { AppComponent } from "./app.component";
 import { AuthenticationComponent } from "./auth/authentication.component";
 import { HeaderComponent } from "./header.component";
@@ -8,9 +15,14 @@ import { routing } from "./app.routing";
 import { AuthService } from "./auth/auth.service";
 import { ErrorComponent } from "./errors/error.component";
 import { ErrorService } from "./errors/error.service";
+import { DataTableModule, SharedModule, DialogModule, DataGridModule, InputTextModule, ButtonModule } from 'primeng/primeng';
+import { TableModule } from 'primeng/table';
+import { Ng4LoadingSpinnerModule } from 'ng4-loading-spinner';
+import { ChartsModule } from 'ng2-charts';
 import { MessageModule } from "./messages/message.module";
 var AppModule = /** @class */ (function () {
-    function AppModule() {
+    function AppModule(appRef) {
+        this.appRef = appRef;
     }
     AppModule.decorators = [
         { type: NgModule, args: [{
@@ -21,15 +33,37 @@ var AppModule = /** @class */ (function () {
                         ErrorComponent
                     ],
                     imports: [
+                        CommonModule,
+                        FormsModule,
                         BrowserModule,
+                        BrowserAnimationsModule,
                         routing,
                         HttpModule,
+                        // PortfolioDetailModule,
+                        Ng4LoadingSpinnerModule.forRoot(),
+                        Ng2OdometerModule.forRoot(),
+                        ChartsModule,
+                        DataTableModule,
+                        DataGridModule,
+                        ButtonModule,
+                        TableModule,
+                        InputTextModule,
+                        SharedModule,
+                        DialogModule,
+                        CalendarModule,
+                        SelectModule,
                         MessageModule
                     ],
-                    providers: [AuthService, ErrorService],
+                    providers: [AuthService, ErrorService
+                    ],
                     bootstrap: [AppComponent]
                 },] },
     ];
+    /** @nocollapse */
+    AppModule.ctorParameters = function () { return [
+        { type: ApplicationRef, },
+    ]; };
     return AppModule;
 }());
 export { AppModule };
+platformBrowserDynamic().bootstrapModule(AppModule);
--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic }  from '@angular/platform-browser-dynamic';
+import { NgModule, ApplicationRef } from '@angular/core';
+import { BrowserModule} from '@angular/platform-browser';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { HttpModule } from "@angular/http";
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Ng2OdometerModule } from 'ng2-odometer';
+import { CalendarModule} from 'primeng/calendar';
+import {SelectModule} from 'ng2-select';
 import { AppComponent } from "./app.component";
 import { AuthenticationComponent } from "./auth/authentication.component";
 import { HeaderComponent } from "./header.component";
@@ -9,24 +15,53 @@ import { routing } from "./app.routing";
 import { AuthService } from "./auth/auth.service";
 import { ErrorComponent } from "./errors/error.component";
 import { ErrorService } from "./errors/error.service";
+import { DataTableModule, SharedModule, DialogModule,DataGridModule ,InputTextModule,ButtonModule} from 'primeng/primeng';
+import { TableModule } from 'primeng/table';
+// import { PortfolioDetailModule } from "./portfolio-detail/portfolio-detail.module";
+import { Ng4LoadingSpinnerModule } from 'ng4-loading-spinner';
+import { ChartsModule } from 'ng2-charts';
+// import { PortfolioService } from "./portfolio/portfolio.service";
+// import { PortfolioComponent } from "./portfolio/portfolio.component";
+// import { PortfolioListComponent } from "./portfolio/portfolio-list.component";
+// import { PortfolioInputComponent } from "./portfolio/portfolio-input.component";
 import { MessageModule } from "./messages/message.module";
 
-@NgModule({
+@NgModule({   
     declarations: [
         AppComponent,
         AuthenticationComponent,
         HeaderComponent,
         ErrorComponent
-    ],
+    ], 
     imports: [
+        CommonModule,
+        FormsModule,
         BrowserModule,
+        BrowserAnimationsModule,
         routing,
         HttpModule,
+        // PortfolioDetailModule,
+        Ng4LoadingSpinnerModule.forRoot(),
+        Ng2OdometerModule.forRoot(),
+        ChartsModule,
+        DataTableModule,
+        DataGridModule,
+        ButtonModule,
+        TableModule,
+        InputTextModule,
+        SharedModule,
+        DialogModule,
+        CalendarModule,
+        SelectModule,
         MessageModule
     ],
-    providers: [AuthService, ErrorService],
+    providers: [AuthService, ErrorService
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
+    constructor(public appRef: ApplicationRef) {
+    }
 
 }
+platformBrowserDynamic().bootstrapModule(AppModule);
